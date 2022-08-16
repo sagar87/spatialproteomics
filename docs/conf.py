921ea7be435c7b9d6614914c15af4c4c68201447
Harald Sager Voehringer <voehring@login.cluster.embl.de>
2022-08-16 15:33:33 +0200

    added rtd docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath(".."))
 
 
@@ -33,6 +33,7 @@ release = "0.1.0"
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,7 +50,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
