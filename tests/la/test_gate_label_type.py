5d5e0331a73e98d4967cffd3ecfc84fb678b320c
Harald Vohringer <harald.voeh@gmail.com>
2023-03-31 16:20:15 +0200

    added second label type to the test_gate_cell_type_adds_graph

--- tests/la/test_gate_label_type.py
+++ tests/la/test_gate_label_type.py
@@ -20,3 +20,27 @@ def test_gate_cell_type_adds_graph(dataset_full):
     assert "label_id" in ds.attrs
     assert "step" in ds.attrs
     assert "num_cells" in ds.attrs
+
+    assert ds.attrs["channel"][1] == "CD4"
+    assert ds.attrs["threshold"][1] == 1e5
+    assert ds.attrs["intensity_key"][1] == "_intensity"
+    assert ds.attrs["override"][1] is False
+    assert ds.attrs["label_name"][1] == "CT1"
+    assert ds.attrs["label_id"][1] == 1
+    assert ds.attrs["step"][1] == 1
+    assert ds.attrs["num_cells"][1] == 626
+
+    # add more cell types
+    ds = ds.la.add_label_type("CT2")
+    ds = ds.la.add_label_type("CT3")
+
+    ds = ds.la.gate_label_type("CT3", "CD8", 2e5, "_intensity")
+
+    assert ds.attrs["channel"][3] == "CD8"
+    assert ds.attrs["threshold"][3] == 2e5
+    assert ds.attrs["intensity_key"][3] == "_intensity"
+    assert ds.attrs["override"][3] is False
+    assert ds.attrs["label_name"][3] == "CT3"
+    assert ds.attrs["label_id"][3] == 3
+    assert ds.attrs["step"][3] == 2
+    assert ds.attrs["num_cells"][3] == 194
