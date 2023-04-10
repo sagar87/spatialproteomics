649ff4376a8efe9248e1a564389ee014bf3492a5
Harald Vohringer <harald.voeh@gmail.com>
2023-04-10 17:20:55 +0200

    test suite for reset logic for label_types

--- tests/la/test_gate_label_type.py
+++ tests/la/test_gate_label_type.py
@@ -1,5 +1,7 @@
 # import pytest
 
+import numpy as np
+
 import spatial_data as sd
 
 # from spatial_data.constants import Dims, Layers
@@ -44,3 +46,199 @@ def test_gate_cell_type_adds_graph(dataset_full):
     assert ds.attrs["label_id"][3] == 3
     assert ds.attrs["step"][3] == 2
     # assert ds.attrs["num_cells"][3] == 194
+
+
+def test_remove_label_type(full_zarr):
+    # tests simple case when there
+
+    cell_type = "Vascular"
+    channel = "CD31"
+    intensity = 0.5
+    intensity_channel = "_transformed"
+    override = False
+    parent = 0
+
+    sdata = full_zarr.la.add_label_type(cell_type, "C0")
+    sdata = sdata.la.gate_label_type(cell_type, channel, intensity, intensity_channel, override, parent)
+
+    assert sdata.attrs["channel"][1] == channel
+    assert sdata.attrs["threshold"][1] == intensity
+    assert sdata.attrs["intensity_key"][1] == intensity_channel
+    assert sdata.attrs["override"][1] is override
+    assert sdata.attrs["label_name"][1] == cell_type
+    assert sdata.attrs["label_id"][1] == 1
+    assert sdata.attrs["step"][1] == 1
+
+    vascular = sdata.la.reset_label_type(cell_type)
+
+    assert 1 not in vascular.attrs["channel"].keys()
+    assert 1 not in vascular.attrs["threshold"].keys()
+    assert 1 not in vascular.attrs["intensity_key"].keys()
+    assert 1 not in vascular.attrs["override"].keys()
+    assert 1 not in vascular.attrs["label_name"].keys()
+    assert 1 not in vascular.attrs["label_id"].keys()
+    assert 1 not in vascular.attrs["step"].keys()
+
+    # more complex scenario with parent cells
+    cell_type = "T cell"
+    channel = "CD3"
+    intensity = 1.1
+    intensity_channel = "_transformed"
+    override = False
+    parent = 0
+
+    sdata = sdata.la.add_label_type(cell_type, "C0")
+    sdata = sdata.la.gate_label_type(cell_type, channel, intensity, intensity_channel, override, parent)
+
+    assert sdata.attrs["channel"][2] == channel
+    assert sdata.attrs["threshold"][2] == intensity
+    assert sdata.attrs["intensity_key"][2] == intensity_channel
+    assert sdata.attrs["override"][2] is override
+    assert sdata.attrs["label_name"][2] == cell_type
+    assert sdata.attrs["label_id"][2] == 2
+    assert sdata.attrs["step"][2] == 2
+
+    num_gated_cells = len(sdata.attrs["gated_cells"][2])
+
+    cell_type = "T cell CD8+"
+    channel = "CD8"
+    # channel_show = ['CD8', 'CD3'] #, 'Granyzme B']
+    intensity = 1.5
+    intensity_channel = "_transformed"
+    override = False
+    parent = "T cell"
+
+    sdata = sdata.la.add_label_type(cell_type, "C1")
+    sdata = sdata.la.gate_label_type(cell_type, channel, intensity, intensity_channel, override, parent)
+    # import pdb
+
+    # pdb.set_trace()
+    assert sdata.attrs["channel"][3] == channel
+    assert sdata.attrs["threshold"][3] == intensity
+    assert sdata.attrs["intensity_key"][3] == intensity_channel
+    assert sdata.attrs["override"][3] is override
+    assert sdata.attrs["label_name"][3] == cell_type
+    assert sdata.attrs["label_id"][3] == 3
+    assert sdata.attrs["step"][3] == 3
+
+    # removing CD8+ T cells should revert them to CD3+ T cells
+
+    sdata = sdata.la.reset_label_type(cell_type)
+
+    assert 3 not in sdata.attrs["channel"].keys()
+    assert 3 not in sdata.attrs["threshold"].keys()
+    assert 3 not in sdata.attrs["intensity_key"].keys()
+    assert 3 not in sdata.attrs["override"].keys()
+    assert 3 not in sdata.attrs["label_name"].keys()
+    assert 3 not in sdata.attrs["label_id"].keys()
+    assert 3 not in sdata.attrs["step"].keys()
+    # checks if the number of CD3+ T cells has been restored
+    assert len(sdata.attrs["gated_cells"][2]) == num_gated_cells
+
+    # add CD8+ T cells again
+    sdata = sdata.la.gate_label_type(cell_type, channel, intensity, intensity_channel, override, parent)
+
+    assert sdata.attrs["channel"][3] == channel
+    assert sdata.attrs["threshold"][3] == intensity
+    assert sdata.attrs["intensity_key"][3] == intensity_channel
+    assert sdata.attrs["override"][3] is override
+    assert sdata.attrs["label_name"][3] == cell_type
+    assert sdata.attrs["label_id"][3] == 3
+    assert sdata.attrs["step"][3] == 3
+
+    # now remove CD3+ T cell (parent class of CD8+ T cells) should
+    # delete all T cells including CD8+ T cells
+
+    sdata = sdata.la.reset_label_type("T cell")
+
+    assert 3 not in sdata.attrs["channel"].keys()
+    assert 3 not in sdata.attrs["threshold"].keys()
+    assert 3 not in sdata.attrs["intensity_key"].keys()
+    assert 3 not in sdata.attrs["override"].keys()
+    assert 3 not in sdata.attrs["label_name"].keys()
+    assert 3 not in sdata.attrs["label_id"].keys()
+    assert 3 not in sdata.attrs["step"].keys()
+
+    assert 2 not in sdata.attrs["channel"].keys()
+    assert 2 not in sdata.attrs["threshold"].keys()
+    assert 2 not in sdata.attrs["intensity_key"].keys()
+    assert 2 not in sdata.attrs["override"].keys()
+    assert 2 not in sdata.attrs["label_name"].keys()
+    assert 2 not in sdata.attrs["label_id"].keys()
+    assert 2 not in sdata.attrs["step"].keys()
+
+    # more complex tree
+    cell_type = "T cell"
+    channel = "CD3"
+    intensity = 1.1
+    intensity_channel = "_transformed"
+    override = False
+    parent = 0
+
+    sdata = sdata.la.gate_label_type(cell_type, channel, intensity, intensity_channel, override, parent)
+
+    cell_type = "T cell CD8+"
+    channel = "CD8"
+    # channel_show = ['CD8', 'CD3'] #, 'Granyzme B']
+    intensity = 1.5
+    intensity_channel = "_transformed"
+    override = False
+    parent = "T cell"
+
+    sdata = sdata.la.gate_label_type(cell_type, channel, intensity, intensity_channel, override, parent)
+
+    cell_type = "T cell CD4+"
+    channel = "CD4"
+    intensity = 1.5
+    intensity_channel = "_transformed"
+    override = False
+    parent = "T cell"
+
+    sdata = sdata.la.add_label_type(cell_type, "C2")
+    sdata = sdata.la.gate_label_type(cell_type, channel, intensity, intensity_channel, override, parent)
+
+    cell_type = "T-reg"
+    channel = "FOXP3"
+    intensity = 0.3
+    intensity_channel = "_transformed"
+    override = False
+    parent = "T cell CD4+"
+
+    sdata = sdata.la.add_label_type(cell_type, "C4")
+    sdata = sdata.la.gate_label_type(cell_type, channel, intensity, intensity_channel, override, parent)
+
+    assert 0 in sdata.attrs["graph"].keys()
+    assert 1 in sdata.attrs["graph"].keys()
+    assert 2 in sdata.attrs["graph"].keys()
+    assert 3 in sdata.attrs["graph"].keys()
+    assert 4 in sdata.attrs["graph"].keys()
+    assert 5 in sdata.attrs["graph"].keys()
+
+    treg_removed = sdata.la.reset_label_type("T-reg")
+
+    assert 0 in treg_removed.attrs["graph"].keys()
+    assert 1 in treg_removed.attrs["graph"].keys()
+    assert 2 in treg_removed.attrs["graph"].keys()
+    assert 3 in treg_removed.attrs["graph"].keys()
+    assert 4 in treg_removed.attrs["graph"].keys()
+    assert 5 not in treg_removed.attrs["graph"].keys()
+    assert treg_removed.attrs["num_cells"][4] == sdata.attrs["num_cells"][4] + sdata.attrs["num_cells"][5]
+    assert np.all(np.isin(sdata.attrs["gated_cells"][5], treg_removed.attrs["gated_cells"][4]))
+
+    cd4_removed = sdata.la.reset_label_type("T cell CD4+")
+
+    assert 0 in cd4_removed.attrs["graph"].keys()
+    assert 1 in cd4_removed.attrs["graph"].keys()
+    assert 2 in cd4_removed.attrs["graph"].keys()
+    assert 3 in cd4_removed.attrs["graph"].keys()
+
+    assert 4 not in cd4_removed.attrs["graph"].keys()
+    assert 5 not in cd4_removed.attrs["graph"].keys()
+
+    assert (
+        cd4_removed.attrs["num_cells"][2]
+        == sdata.attrs["num_cells"][2] + sdata.attrs["num_cells"][4] + sdata.attrs["num_cells"][5]
+    )
+
+    assert np.all(np.isin(sdata.attrs["gated_cells"][5], cd4_removed.attrs["gated_cells"][2]))
+    assert np.all(np.isin(sdata.attrs["gated_cells"][4], cd4_removed.attrs["gated_cells"][2]))
