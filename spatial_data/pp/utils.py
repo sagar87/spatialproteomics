d2d181efc31d8253655133da754bfc46f5540e97
Harald Vohringer <harald.voeh@gmail.com>
2024-04-11 13:42:24 +0200

    removed unnecessary variable

--- spatial_data/pp/utils.py
+++ spatial_data/pp/utils.py
@@ -189,7 +189,7 @@ def _relabel_cells(segmentation: np.ndarray):
     The mapping dictionary provides a mapping from the original values to the new values.
     """
     unique_values = np.unique(segmentation)  # Find unique values in the array
-    num_unique_values = len(unique_values)  # Get the number of unique values
+    # num_unique_values = len(unique_values)  # Get the number of unique values
 
     # Create a mapping from original values to new values
     value_map = {value: i for i, value in enumerate(unique_values)}
