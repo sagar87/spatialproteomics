9156c3e6188425b4139f1298884ca60ab30ea2de
Matthias Fabian Meyer-Bender <matthias.meyerbender@embl.de>
2024-06-12 10:25:10 +0200

    added warning to astir when type is not uint8 instead of error

--- spatialproteomics/la/label.py
+++ spatialproteomics/la/label.py
@@ -12,7 +12,7 @@ from ..constants import Dims, Features, Layers, Props
 @xr.register_dataset_accessor("la")
 class LabelAccessor:
     """Adds functions for cell phenotyping."""
-    
+
     def __init__(self, xarray_obj):
         self._obj = xarray_obj
 
