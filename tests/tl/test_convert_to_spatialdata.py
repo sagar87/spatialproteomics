0ba9ac327e199ad9b0bcb304bbfe9147b6171449
Matthias Fabian Meyer-Bender <matthias.meyerbender@embl.de>
2025-04-14 13:59:09 +0200

    added some more tests

--- tests/tl/test_convert_to_spatialdata.py
+++ tests/tl/test_convert_to_spatialdata.py
@@ -1,14 +1,14 @@
 def test_convert_to_spatialdata_image(ds_image):
-    ds_image.tl.convert_to_anndata()
+    ds_image.tl.convert_to_spatialdata()
 
 
 def test_convert_to_spatialdata_segmentation(ds_segmentation):
-    ds_segmentation.tl.convert_to_anndata()
+    ds_segmentation.tl.convert_to_spatialdata()
 
 
 def test_convert_to_spatialdata_labels(ds_labels):
-    ds_labels.tl.convert_to_anndata()
+    ds_labels.tl.convert_to_spatialdata()
 
 
 def test_convert_to_spatialdata_neighborhoods(ds_neighborhoods):
-    ds_neighborhoods.tl.convert_to_anndata()
+    ds_neighborhoods.tl.convert_to_spatialdata()
