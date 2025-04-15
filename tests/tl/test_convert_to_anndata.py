792a8d1c4550fa6ddda8ad34e748aeb00ee6ae0f
Matthias Fabian Meyer-Bender <matthias.meyerbender@embl.de>
2025-04-15 16:08:32 +0200

    added functional tests for export functions

--- tests/tl/test_convert_to_anndata.py
+++ tests/tl/test_convert_to_anndata.py
@@ -1,14 +1,33 @@
 def test_convert_to_spatialdata_image(ds_image):
-    ds_image.tl.convert_to_anndata()
+    adata = ds_image.tl.convert_to_anndata()
+    assert adata.X is None
 
 
 def test_convert_to_spatialdata_segmentation(ds_segmentation):
-    ds_segmentation.tl.convert_to_anndata()
+    adata = ds_segmentation.tl.convert_to_anndata()
+    assert adata.X is None
 
 
 def test_convert_to_spatialdata_labels(ds_labels):
-    ds_labels.tl.convert_to_anndata()
+    adata = ds_labels.tl.convert_to_anndata()
+    assert adata.X.shape == (56, 5)
+    assert "CD4_binarized" in adata.obs.columns
+    assert "CD8_binarized" in adata.obs.columns
+    assert "_labels" in adata.obs.columns
+    assert "centroid-0" in adata.obs.columns
+    assert "centroid-1" in adata.obs.columns
+    assert "_labels_colors" in adata.uns.keys()
+    assert "spatial" in adata.obsm.keys()
 
 
 def test_convert_to_spatialdata_neighborhoods(ds_neighborhoods):
-    ds_neighborhoods.tl.convert_to_anndata()
+    adata = ds_neighborhoods.tl.convert_to_anndata()
+    assert adata.X.shape == (56, 5)
+    assert "_neighborhoods" in adata.obs.columns
+    assert "CD4_binarized" in adata.obs.columns
+    assert "CD8_binarized" in adata.obs.columns
+    assert "_labels" in adata.obs.columns
+    assert "centroid-0" in adata.obs.columns
+    assert "centroid-1" in adata.obs.columns
+    assert "_labels_colors" in adata.uns.keys()
+    assert "spatial" in adata.obsm.keys()
